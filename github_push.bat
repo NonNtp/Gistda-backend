ab9ae3224bea438fcc452a058f3930394e41914f
nantapongdoa <nantapongdoa@cpall.co.th>
2023-04-20 00:29:06 +0700

    2023 0Th_002906

--- github_push.bat
+++ github_push.bat
@@ -1,7 +1,16 @@
-#!/bin/bash
-
 @echo off
+setlocal enableextensions
+set "YYYY=%date:~-4%"
+set "MM=%date:~3,2%"
+set "DD=%date:~0,2%"
+set "HH=%time:~0,2%"
+if "%HH:~0,1%"==" " set "HH=0%HH:~1,1%"
+set "MIN=%time:~3,2%"
+if "%MIN:~0,1%"==" " set "MIN=0%MIN:~1,1%"
+set "SEC=%time:~6,2%"
+if "%SEC:~0,1%"==" " set "SEC=0%SEC:~1,1%"
+set "timestamp=%YYYY%%MM%%DD%_%HH%%MIN%%SEC%"
 cd /d "C:/Users/darkn/Downloads/GISTDA/Spring/cspp"
 git add .
-git commit -m "Automated commit $(date +'%Y-%m-%d %H:%M:%S')"
-git push origin main
+git commit -m "%timestamp%"
+git push origin master
